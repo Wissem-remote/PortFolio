9611f614beee4014b40da7e7ce09542e31e2d12b
rezki wissem <wissemrezki@gmail.com>
2022-08-05 11:31:10 +0200

    cv

--- src/Content/Home.js
+++ src/Content/Home.js
@@ -60,7 +60,7 @@ export const Home=()=> {
             <ul className="list-inline">
                 <motion.li  transition={{ type: "tween", duration: 0.5 }} whileHover={{ scale: 1.1, y: -10 }} className="list-inline-item"><a href="https://github.com/Wissem-remote" ><i className="bi bi-github" style={{fontSize: '2rem'}}></i></a></motion.li>
                 <motion.li transition={{ type: "tween", duration: 0.5 }} whileHover={{ scale: 1.1, y: -10 }} className="list-inline-item">  <a href="https://www.linkedin.com/in/wissem-rezki-3b4889231/"><i className="bi bi-linkedin" style={{fontSize: '2rem'}}></i></a></motion.li>
-                <motion.li transition={{ type: "tween", duration: 0.5 }} whileHover={{ scale: 1.1, y: -10 }} className="list-inline-item">  <a href="https://www.doyoubuzz.com/wissem-rezki"><i className="bi bi-link-45deg" style={{fontSize: '2rem'}}></i></a></motion.li>
+                <motion.li transition={{ type: "tween", duration: 0.5 }} whileHover={{ scale: 1.1, y: -10 }} className="list-inline-item">  <a href="https://media.graphassets.com/oqzjjHeWTg2HfWpKDmNN"><i className="bi bi-link-45deg" style={{fontSize: '2rem'}}></i></a></motion.li>
             </ul>
             </motion.div>
         </div>
