6fe04790481a23e7f883d6955b950745061d5952
rezki wissem <wissemrezki@gmail.com>
2023-01-06 20:31:00 +0100

    portfolio tests

--- src/Content/Skils.js
+++ src/Content/Skils.js
@@ -27,29 +27,29 @@ export const Skils=()=> {
 
                                 <div className="row d-flex justify-content-center">
                                 <Box value="HTML/CSS">
-                                <i className='bx bxl-html5   ' style={{fontSize:'95px'}}></i>
+                                <i className='bx bxl-html5' style={{fontSize:'95px'}}></i>
                                 </Box>
                                 
                                 <Box value="Javascript">
-                                <i className='bx bxl-javascript '  style={{fontSize:'95px'}} ></i>
+                                <i className='bx bxl-javascript'  style={{fontSize:'95px'}} ></i>
 
                                 </Box>
                                 <Box value="Php">
-                                <i className='bx bxl-php ' style={{fontSize:'95px'}} ></i>
+                                <i className='bx bxl-php' style={{fontSize:'95px'}} ></i>
                                 </Box>
 
                                 <Box value="React">
-                                <i className='bx bxl-react ' style={{fontSize:'95px'}} ></i>
+                                <i className='bx bxl-react' style={{fontSize:'95px'}} ></i>
                                 </Box>
                                 <Box value="Symfony">
-                                <i className='bx bxl-php bx-tada ' style={{fontSize:'95px'}}></i>
+                                <i className='bx bxl-php bx-tada' style={{fontSize:'95px'}}></i>
                                 </Box>
                                 <Box value="MySql">
-                                <i className='bx bxs-data ' style={{fontSize:'95px'}} ></i>
+                                <i className='bx bxs-data' style={{fontSize:'95px'}} ></i>
                                 
                                 </Box>
                                 <Box value="Bootstrap">
-                                <i className='bx bxl-bootstrap ' style={{fontSize:'95px'}} ></i>
+                                <i className='bx bxl-bootstrap' style={{fontSize:'95px'}} ></i>
                                 </Box>
                                 <Box value="Tailwind">
                                 <i className='bx bxl-tailwind-css bx-spin' style={{fontSize:'95px'}} ></i>
